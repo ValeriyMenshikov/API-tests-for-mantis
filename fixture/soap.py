a1c8418228f5e7a4c1051b9fde07227cc92fd5f2
Valeriy Menshikov <team095hunter@gmail.com>
2020-03-17 21:04:30 +0600

    Fix bug in soap fixture and create requirements

--- fixture/soap.py
+++ fixture/soap.py
@@ -10,7 +10,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -18,7 +18,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             group_list = client.service.mc_projects_get_user_accessible(username, password)
             groups = []
